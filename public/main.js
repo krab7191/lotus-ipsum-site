3d878d4015f44df4ec59491b365675576632c71b
Karsten Rabe <karstenrabe91@gmail.com>
2019-07-25 19:46:06 -0400

    Add clipboard support and notification

--- public/main.js
+++ public/main.js
@@ -15,4 +15,31 @@ function appendToPage(text) {
 	const cont = document.getElementById('text-container');
 	cont.innerText = text.trim();
 	cont.style.display = 'block';
+	document.getElementById('copy-button').click();
 }
+
+function clipboardMsg(success) {
+	const cont = document.getElementById('message-container');
+	cont.innerText = success ? 'Copied to clipboard!' : 'Error copying!';
+	cont.style.color = success ? 'green' : 'red';
+	cont.style.visibility = 'visible';
+	window.setTimeout(function () {
+		cont.innerText = '';
+		cont.style.color = 'white';
+		cont.style.visibility = 'hidden';
+	}, 2000);
+}
+
+(function () {
+	const clippy = new ClipboardJS('#copy-button');
+
+	clippy.on('success', function (e) {
+		e.clearSelection();
+		clipboardMsg(true);
+	});
+
+	clippy.on('error', function (e) {
+		e.clearSelection();
+		clipboardMsg(false);
+	});
+})();
