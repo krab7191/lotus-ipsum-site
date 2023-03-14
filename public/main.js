4673ace74f27475e26f1c31ad5f20eca53b4780d
Karsten Rabe <karstenrabe91@gmail.com>
2023-03-14 12:22:02 -0400

    audit and format

--- public/main.js
+++ public/main.js
@@ -1,50 +1,52 @@
-
 function getParagraphs() {
-	let numParas = document.getElementById('num-paras').value;
-	numParas.length === 0 && (numParas = 3);
-	numParas === '0' && (numParas = 3);
-	console.log(`GET ${numParas} paras`);
-	axios.get(`/api/ipsum/${numParas}`).then(({ data }) => {
-		appendToPage(data.text + "");
-	}).catch(err => {
-		console.log(err);
-	});
+  let numParas = document.getElementById("num-paras").value;
+  numParas.length === 0 && (numParas = 3);
+  numParas === "0" && (numParas = 3);
+  console.log(`GET ${numParas} paras`);
+  axios
+    .get(`/api/ipsum/${numParas}`)
+    .then(({ data }) => {
+      appendToPage(data.text + "");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function appendToPage(text) {
-	const cont = document.getElementById('text-container');
-	cont.innerText = text.trim();
-	cont.style.display = 'block';
-	document.getElementById('copy-button').click();
+  const cont = document.getElementById("text-container");
+  cont.innerText = text.trim();
+  cont.style.display = "block";
+  document.getElementById("copy-button").click();
 }
 
 function clipboardMsg(success) {
-	const cont = document.getElementById('message-container');
-	cont.innerText = success ? 'Copied to clipboard!' : 'Error copying!';
-	cont.style.color = success ? 'green' : 'red';
-	cont.style.visibility = 'visible';
-	window.setTimeout(function () {
-		cont.innerText = '';
-		cont.style.color = 'white';
-		cont.style.visibility = 'hidden';
-	}, 2000);
+  const cont = document.getElementById("message-container");
+  cont.innerText = success ? "Copied to clipboard!" : "Error copying!";
+  cont.style.color = success ? "green" : "red";
+  cont.style.visibility = "visible";
+  window.setTimeout(function () {
+    cont.innerText = "";
+    cont.style.color = "white";
+    cont.style.visibility = "hidden";
+  }, 2000);
 }
 
 (function () {
-	const clippy = new ClipboardJS('#copy-button');
+  const clippy = new ClipboardJS("#copy-button");
 
-	clippy.on('success', function (e) {
-		e.clearSelection();
-		clipboardMsg(true);
-	});
+  clippy.on("success", function (e) {
+    e.clearSelection();
+    clipboardMsg(true);
+  });
 
-	clippy.on('error', function (e) {
-		e.clearSelection();
-		clipboardMsg(false);
-	});
+  clippy.on("error", function (e) {
+    e.clearSelection();
+    clipboardMsg(false);
+  });
 
-	document.onkeyup = function (e) {
-		const key = e.keyCode;
-		key === 13 && getParagraphs();
-	}
+  document.onkeyup = function (e) {
+    const key = e.keyCode;
+    key === 13 && getParagraphs();
+  };
 })();
